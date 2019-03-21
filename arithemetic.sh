f549a752666b88d54ffac7f44424a3543f197984
shravanrre <shravanrre7@gmail.com>
2019-03-21 19:42:41 +0530

    committed

---
+++ arithemetic.sh
@@ -0,0 +1,15 @@
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+ 
+sum=$((num1+num2))
+ 
+diff=$((num1-num2))
+ 
+division=$((num1/num2))
+ 
+product=$((num1*num2))
+ 
+echo "Sum is: $sum"
+echo "diff is: $diff"
+echo "division is: $division"
+echo "product is: $product"    
